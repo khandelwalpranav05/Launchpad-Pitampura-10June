1f0a961a01bd7168b49f8b2939b3a6cbcf9e1af6
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-06-25 19:35:02 +0530

    lecture 8

--- Lecture 8/recursion_basics.cpp
+++ Lecture 8/recursion_basics.cpp
@@ -23,9 +23,103 @@ int factorial(int n){
 	return total_fact;
 }
 
+int fibonacci(int n){
+	if(n==1 || n==0){
+		return n;
+	}
+
+	int fn1 = fibonacci(n-1);
+	int fn2 = fibonacci(n-2);
+
+	int fn = fn1 + fn2;
+	return fn;
+}
+
+void printD(int n){
+	if(n==0){
+		return;
+	}
+
+	cout<<n<<endl;
+	printD(n-1);
+}
+
+void printI(int n){
+	if(n==0){
+		return;
+	}
+
+	printI(n-1);
+	cout<<n<<endl;
+}
+
+void printID(int n){
+	if(n==0){
+		return;
+	}
+
+	cout<<n<<endl;
+	printID(n-1);
+	cout<<n<<endl;
+}
+
+int power(int n,int p){
+    if(p==0){
+        return 1;
+    }
+
+    int powern1 = power(n,p-1);
+    int total_power = n*powern1;
+    return total_power;
+}
+
+//bool isSorted(int arr[],int size,int idx){
+//
+//    if(idx==(size-1)){
+//        return true;
+//    }
+//
+//    bool restArr = isSorted(arr,size,idx+1);
+//
+//    if(arr[idx]<arr[idx+1] and restArr==true){
+//        return true;
+//    }
+//
+//    return false;
+//}
+
+bool isSorted(int *arr,int size){
+    if(size==1){
+        return true;
+    }
+
+    bool restArr = isSorted(arr+1,size-1);
+
+    if(arr[0]<arr[1] and restArr){
+        return true;
+    }
+
+    return false;
+}
+
 int main(){
 
 	// cout<<sumTilln(4)<<endl;
+
 	// cout<<factorial(5)<<endl;
+
+	// cout<<fibonacci(4)<<endl;
+
+	// printD(5);
+	// printI(5);
+	// printID(5);
+
+	// int n=5;
+ //   	int p = 3;
+	// cout<<power(n,p)<<endl;
+
+    // int arr[] = {1,2,3,4,5};
+    // cout<<isSorted(arr,5)<<endl;
+
 	return 0;
 }
