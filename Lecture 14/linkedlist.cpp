c24c27d0ee76ae690b6be99bc0be76a29216b0e7
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-07-11 16:53:56 +0530

    LinkedList continued

--- Lecture 14/linkedlist.cpp
+++ Lecture 14/linkedlist.cpp
@@ -13,13 +13,13 @@ public:
 	}
 };
 
-void insertAtHead(node*&head,int data){
+void insertAtHead(node*&newHead,int data){
 	node* n = new node(data);
-	n->next = head;
-	head = n;
+	n->next = newHead;
+	newHead = n;
 }
 
-void display(node* head){
+void display(node*&head){
 	node* temp = head;
 
 	while(temp!=NULL){
@@ -46,24 +46,131 @@ void insertAtTail(node*&head,int data){
 
 }
 
+int length(node* head){
+	int count = 0;
+
+	while(head!=NULL){
+		head = head->next;
+		count++;
+	}
+
+	return count;
+}
+
+void insertAtIndex(node*&head,int data,int pos){
+	if(pos==0 || head==NULL || pos==1){
+		insertAtHead(head,data);
+	}
+
+	if(pos>length(head)){
+		insertAtTail(head,data);
+	}
+
+	node* temp = head;
+
+	int count = 1;
+
+	while(count<pos-1){
+		temp = temp->next;
+		count++;
+	}
+
+	node* n = new node(data);
+
+	n->next = temp->next;
+	temp->next = n;
+}
+
+void deleteAtHead(node*&head){
+	node* temp = head;
+	head = head->next;
+
+	delete temp;
+}
+
+void deleteAtTail(node*head){
+	node* prev = NULL;
+	node* temp = head;
+
+	while(temp->next!=NULL){
+		prev = temp;
+		temp = temp->next;
+	}
+
+	delete temp;
+	prev->next = NULL;
+}
+
+bool searchIterative(node* head,int item){
+	node* temp = head;
+
+	while(temp!=NULL){
+		if(temp->data==item){
+			return true;
+		}
+		temp = temp->next;
+	}
+
+	return false;
+}
+
+bool searchRecursive(node* head,int item){
+	if(head==NULL){
+		return false;
+	}
+
+	if(head->data==item){
+		return true;
+	}else{
+		return searchRecursive(head->next,item);
+	}
+}
+
 int main(){
 
 	node* head = NULL;
 
-	insertAtTail(head,12);
+	// insertAtTail(head,12);
+	// display(head);
+
+	// insertAtHead(head,2);
+	// display(head);
+
+	insertAtHead(head,4);
+	insertAtHead(head,3);
+	insertAtHead(head,2);
+	insertAtHead(head,1);
+
 	display(head);
 
-	// insertAtHead(head,4);
-	// insertAtHead(head,3);
-	// insertAtHead(head,2);
-	// insertAtHead(head,1);
+	insertAtTail(head,5);
+	insertAtTail(head,6);
 
-	// display(head);
+	display(head);
 
-	// insertAtTail(head,5);
-	// insertAtTail(head,6);
+	insertAtIndex(head,789,3);
 
-	// display(head);
+	display(head);
+
+	deleteAtHead(head);
+
+	display(head);
+
+	deleteAtTail(head);
+
+	display(head);
+
+	// if(searchIterative(head,3)){
+	// 	cout<<"Found!"<<endl;
+	// }else{
+	// 	cout<<"Not found!"<<endl;
+	// }
+
+	if(searchRecursive(head,3)){
+		cout<<"Found!"<<endl;
+	}else{
+		cout<<"Not found!"<<endl;
+	}
 
 	return 0;
 }
@@ -102,3 +209,10 @@ int main(){
 // 	// cout<<"Value of a inside main is "<<a<<endl;
 // 	return 0;
 // }
+
+// Stacks and queues (1)
+// Binary Tree BST (2)
+// HashMap & Heaps (2)
+// Tries (1)
+// Graph (2)
+// DP (2)
