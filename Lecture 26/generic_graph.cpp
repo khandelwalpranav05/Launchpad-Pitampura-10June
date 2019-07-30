c062d39cc0944768c3805b18252b53c58d6b6591
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-07-31 03:34:28 +0530

    graph

--- Lecture 26/generic_graph.cpp
+++ Lecture 26/generic_graph.cpp
@@ -168,17 +168,3 @@ int main(){
 	return 0;
 }
 
-
-
-	// int board[50] = {0};
-
-	// board[2] = 13;
-	// board[5] = 2;
-	// board[9] = 18;
-	// board[18] = 11;
-	// board[17] = -13;
-	// board[20] = -14;
-	// board[24] = -8;
-	// board[25] = -10;
-	// board[32] = -2;
-	// board[34] = -22;
