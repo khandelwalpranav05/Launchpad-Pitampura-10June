79e6603d210dd4f001fa98b1ba2b61f3cb937ca7
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-07-30 17:39:35 +0530

    generic graph

--- Lecture 26/generic_graph.cpp
+++ Lecture 26/generic_graph.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
+#include <list>
+#include <map>
 
 using namespace std;
 
-
+template <typename T>
 class Graph{
-	map<> adjList;
-}
+	map<T, list<T> > adjList;
+
+public:
+	void addEdge(T u,T v, bool bidir = true){
+		adjList[u].push_back(v);
+
+		if(bidir){
+			adjList[v].push_back(u);
+		}
+	}
+
+	void display(){
+		for(auto vertex:adjList){
+			cout<<vertex.first<<" -> ";
+
+			for(auto neighbour:vertex.second){
+				cout<<neighbour<<",";
+			}
+			cout<<endl;
+		}
+	}
+};
 
 int main(){
+
+	Graph<int> g;
+
+	g.addEdge(1,2);
+	g.addEdge(1,3);
+	g.addEdge(1,5);
+	g.addEdge(2,3);
+	g.addEdge(3,4);
+	g.addEdge(4,5);
+
+	g.display();
+
 	return 0;
 }
